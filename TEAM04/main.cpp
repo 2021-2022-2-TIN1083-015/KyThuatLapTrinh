838f14a609fa566a1ff64e989bd797d2981c7dd6
Vo Viet Dung <vvdung@hueuni.edu.vn>
2022-01-19 14:43:42 +0700

    vvdung

---
+++ TEAM04/main.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void Member_01(){
+
+}
+void Member_02(){
+  
+}
+void Member_03(){
+  
+}
+
+int main(int argc, const char** argv) {
+  printf("WELCOME C/C++!!!\n");
+  Member_01();
+  Member_02();
+  Member_03();
+  return 0;
+}
