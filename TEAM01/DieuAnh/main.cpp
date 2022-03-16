134ab3fcb920ca432ff7c1628fe91f8da2d0e915
Nguyen Thi Dieu Anh <21T1020113@husc.edu.vn>
2022-03-16 13:57:50 +0700

    DieuAnh

--- TEAM01/DieuAnh/main.cpp
+++ TEAM01/DieuAnh/main.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main(){
   printf("Welcome KTLT\n");
   LOG_ET("Hi\n");
-  LOG_WT("Hello C/C++\n");
-  LOG_IT("Bye\n");
-  LOG_DT("Hi\n");
-  LOG_E("Halo\n");
-  LOG_W("Hello F\n");
+  LOG_WT("Nguyen\n");
+  LOG_IT("Thi\n");
+  LOG_DT("Dieu\n");
+  LOG_E("Anh\n");
+  LOG_W("Halo F\n");
   LOG_I("Welcom \n");
   LOG_D("Bye\n");
 
