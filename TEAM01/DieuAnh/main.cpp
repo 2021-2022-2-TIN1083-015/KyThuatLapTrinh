dcfb7bc59302d8151911ec5c393552f5e0b48b68
Nguyen Thi Dieu Anh <21T1020113@husc.edu.vn>
2022-03-16 13:52:21 +0700

    DieuANh

---
+++ TEAM01/DieuAnh/main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "..\..\_src\Log.h"
+using namespace std;
+
+int main(){
+  printf("Welcome KTLT\n");
+  LOG_ET("Welcom A\n");
+  LOG_WT("Welcom B\n");
+  LOG_IT("Welcom C\n");
+  LOG_DT("Welcom D\n");
+  LOG_E("Welcom E\n");
+  LOG_W("Welcom F\n");
+  LOG_I("Welcom G\n");
+  LOG_D("Welcom H\n");
+
+  return 0;
+}
