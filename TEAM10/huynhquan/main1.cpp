de9e1d53718292c62777394c2c2efe981deece37
Huỳnh Quân <huynhquan19111995@gmail.com>
2022-03-16 15:34:50 +0700

    a

--- TEAM10/huynhquan/main1.cpp
+++ TEAM10/huynhquan/main1.cpp
@@ -16,8 +16,6 @@ std::string name =StringFormat("%s %s %02d %02d %04d",ten,lop,ngay,thang,nam);
 LOG_IT("%s\n",name.c_str());
 std::vector<std::string> arrName = SplitString(name.c_str()," ");
 int iCnt = (int)arrName.size();
-for (int i=0; i<iCnt; i++){
+for (int i=0; i<iCnt; i++)
     LOG_WT("%d [%s] \n",i,arrName[i].c_str());
-    }
-    return 0;
 }
