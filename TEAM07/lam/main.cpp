c418dc620bd6a405470b6d6afa507870b290a150
Your Name <21t1020465@husc.edu.vn>
2022-03-24 00:25:05 +0700

    lam

---
+++ TEAM07/lam/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include "..\..\_src\Log.h"
+using namespace std;
+
+int main(const char* col,const char* fmt){
+    cout<<"you hurt me"<<endl;
+    LOG_ET("let go\n");
+    LOG_WT("color \n");
+    LOG_IT("rain C\n");
+    LOG_DT("end\n");
+    LOG_E("let go\n");
+    LOG_W("color\n");
+    LOG_I("rain\n");
+    LOG_D("end\n");
+    
+    return 0;
+}
