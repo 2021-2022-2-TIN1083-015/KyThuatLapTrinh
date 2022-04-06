dd5ded14c214864c5ff36e7975b6cf0c4a926b03
Duong Minh Chau <21T1020119@husc.edu.vn>
2022-04-06 16:09:51 +0700

    mchau add

--- TEAM01/MinhChau/KTLT.cpp
+++ TEAM01/MinhChau/KTLT.cpp
@@ -5,30 +5,32 @@
 #include "../../_src/Utils.h"
 using namespace std;
 void solve(string s){
-	int sum = 0;
-	for(int i = 0; i < s.size(); i++){
-	if(s[i] >= '0' && s[i] <='9'){
-		int dem = 0;
-		for(int j = i; j < s.size(); j++){
-			if(s[j] >= '0' && s[j] <='9'){
-				dem++;
-			}else{
-				break;
+	long int tong = 0;
+	int len = s.size();
+	for(int i = 0; i < len ; i++){
+		if(s[i] >= 48 && s[i] <= 57){
+			int count = 0;
+			for(int j = i; j < len; j++){
+				if(s[j] >= 48 && s[j] <= 57){
+					count++;
+				}else{
+					break;
+				}
 			}
+		tong = tong + (s[i] - 48) * pow(10,count-1);
 		}
-		sum = sum + (s[i] - 48)*pow(10,dem-1);
 	}
+cout<<tong;
 }
-cout<<sum;
-}
+
 int main()
 {
     string s; getline(cin,s);
     string tong="Bai: Tong so trong xau";
     LOG_WT("%s\n",tong.c_str());
-    string ten="Ten: Nguyen Dinh Ngoc Bao";
+    string ten="Ten: Duong Minh Chau";
     LOG_IT("%s\n",ten.c_str());
-    string msv="Msv: 21T1020237";
+    string msv="Msv: 21T1020119";
     LOG_IT("%s\n",msv.c_str());
     string cach="-----";
     LOG_DT("%s\n",cach.c_str());
