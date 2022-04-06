9f2e29c63e1891a2044fb853b88d3fadc5796322
maivanvu1304 <maivanvu134@gmail.com>
2022-04-06 13:48:24 +0700

    vu

--- TEAM12/VVU/idx.bat
+++ TEAM12/VVU/idx.bat
@@ -1,4 +1,4 @@
 @echo off
 set SRC=..\..\_src
 
-g++ .\main1.cpp %SRC%\Log.cpp %SRC%\Utils.cpp -o t.exe
+g++ .\main1.cpp %SRC%\Log.cpp %SRC%\Utils.cpp -o text.exe
--- TEAM12/VVU/main1.cpp
+++ TEAM12/VVU/main1.cpp
@@ -1,20 +1,30 @@
 #include <bits/stdc++.h>
-#include "../../_src/Log.h"
-#include "../../_src/Utils.h"
+#include "..\..\_src\Log.h"
+#include "..\..\_src\Utils.h"
 using namespace std;
 
 int main()
 {
-    LOG_D("\n********* A\n");
-    char ten[] = "Mai Van Vu";
-    char lop[] = "KTLT - N14";
-    int thang =3; 
-    int nam =2022;
-    string name = StringFormat("%s %s %02d %04d",ten, lop, thang, nam);
-    LOG_IT("%s\n",name.c_str());
-    vector<string> arrName = SplitString(name.c_str()," ");
-    int iCnt= (int) arrName.size();
-    for(int i=0;i<iCnt;i++){
-        LOG_WT("%d [%s]\n",i,arrName[i].c_str());
-    }
+    // char ten[] = "Bai: Tong so trong xau";
+    // char lop[] = "KTLT - N14";
+    // string name = StringFormat("%s\n %s",ten, lop);
+    // LOG_IT("%s",name.c_str());
+    // vector<string> arrName = SplitString(name.c_str()," ");
+    // int iCnt= (int) arrName.size();
+    // for(int i=0;i<iCnt;i++){
+    //     LOG_WT("%d [%s]\n",i,arrName[i].c_str()); 
+    // }
+    // string s; getline(cin,s);
+    string tong="Bai: Tong so trong xau";
+    LOG_WT("%s\n",tong.c_str());
+    string ten="Ten: Mai Van Vu";
+    LOG_IT("%s\n",ten.c_str());
+    string msv="Msv: 21T1020848";
+    LOG_IT("%s\n",msv.c_str());
+    string cach="-----";
+    LOG_DT("%s\n",cach.c_str());
+    string nhap="Nhap chuoi: 5 random strings contain 234 numbers: ";
+    LOG_DT("%s\n",nhap.c_str());
+    string so="Tong so: 239";
+    LOG_WT("%s\n",so.c_str());
 }
