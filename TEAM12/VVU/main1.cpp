3c82f80f5eb7288c9d0923234bde830a9154009e
Your Name <you@example.com>
2022-04-20 06:51:48 +0000

    vu

--- TEAM12/VVU/main1.cpp
+++ TEAM12/VVU/main1.cpp
@@ -18,7 +18,7 @@ int main(){
  	}
  	if(nho>0)
  	s=char(nho+'0')+s;
-    string tong="Bai: Tong so trong xau";
+    string tong="Bai: Cong hai so nguyen lon";
     LOG_WT("%s\n",tong.c_str());
     string ten="Ten: Mai Van Vu";
     LOG_IT("%s\n",ten.c_str());
@@ -35,4 +35,5 @@ int main(){
     string so="Tong so: ";
     LOG_WT("%s",so.c_str());
     cout<<s;
+	cout<<endl;
 }
