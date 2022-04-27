d837530d8033c6c739e5100054ad5f192f8422dd
Nguyen Thi Dieu Anh <21T1020113@husc.edu.vn>
2022-04-27 13:46:23 +0700

    DieuAnh

--- TEAM01/DieuAnh/CongHaiSoNguyenLon.cpp
+++ TEAM01/DieuAnh/CongHaiSoNguyenLon.cpp
@@ -1,31 +1,30 @@
-#include <bits/stdc++.h>
+#include<iostream>
 #include "../../_src/Log.h"
 #include "../../_src/Utils.h"
 #include <string>
 using namespace std;
 
 int main(){
-    string x1, x2, ketqua;
-	int nho=0, kq=0;
-	cin >> x1 >> x2;  
-    LOG_WT("%s\n",tong.c_str());
-		string ten="Ten: Nguyen Thi Dieu Anh";
-    LOG_IT("%s\n",ten.c_str());
-	    string msv="Msv: 21T1020113";
-    LOG_IT("%s\n",msv.c_str());
-        string cach="-----";
-    LOG_DT("%s\n",cach.c_str());
-	    string nhapx1="Nhap so x1: ";
-    LOG_DT("%s",nhapx1.c_str());
-	string x1;
-	cin>>x1;
-	    string nhapx2="Nhap so x2: ";
-    LOG_DT("%s",nhapx2.c_str());
-    string x2;
-    cin>>x2;
-    string so="Tong so: ";
-    LOG_WT("%s",so.c_str());
-    string ket_qua;
+    int nho = 0, kq = 0;
+        string tong = "Bai: Cong hai so nguyen lon";
+    LOG_WT("%s\n", tong.c_str());
+        string ten = "Ten: Nguyen Thi Dieu Anh";
+    LOG_IT("%s\n", ten.c_str());
+        string msv = "Msv: 21T1020113";
+    LOG_IT("%s\n", msv.c_str());
+        string cach = "-----";
+    LOG_DT("%s\n", cach.c_str());
+        string nhapx1 = "Nhap so x1: ";
+    LOG_DT("%s", nhapx1.c_str());
+        string x1;
+        cin >> x1;
+        string nhapx2 = "Nhap so x2: ";
+    LOG_DT("%s", nhapx2.c_str());
+        string x2;
+        cin >> x2;
+        string so = "Tong so: ";
+    LOG_WT("%s", so.c_str());
+        string ketqua;
 	while (x1.size() < x2.size())	
 		x1 = '0' + x1; 
 	while (x2.size() < x1.size())	
