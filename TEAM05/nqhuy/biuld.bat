0eea70e25a523b34aef5e9a7dc33954cc476dcc1
Nguyen Quoc Huy <21T1080017@husc.edu.vn>
2022-05-04 15:55:23 +0700

    nqhuy

--- TEAM05/nqhuy/biuld.bat
+++ TEAM05/nqhuy/biuld.bat
@@ -1,7 +1,14 @@
 @echo off
-set SRC=..\..\_src\
+set SRC=..\..\_src
+set LIB=D:\msys64\mingw64\lib\
 
-g++ .\baitap1.cpp ^
+g++ .\time.cpp ^
     %SRC%\Log.cpp ^
     %SRC%\Utils.cpp ^
--o t.exe
+    %SRC%\HttpClient.cpp ^
+-DCURL_STATICLIB ^
+-o t.exe ^
+-static -L%LIB% ^
+-lcurl -lnghttp2 -lidn2 -lpsl -lunistring -liconv ^
+-lssh2 -lssl -lcrypto -lzstd -lbrotlidec -lbrotlicommon -lz ^
+-ladvapi32 -lcrypt32 -lgdi32 -lwldap32 -lws2_32
