4bfa7f25535aee559071398d7598b6424262100b
Loc1907 <nguyenvanloc1907@gmail.com>
2022-05-21 17:40:00 +0700

    a

---
+++ TEAM07/loc/kthk.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include "../../_src/Log.h"
+#include "../../_src/Utils.h"
+using namespace std;
+
+int main(){
+	string j;
+    ios::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+	LOG_WT("Bai: Ma hoa van ban\n");
+    LOG_IT("Ten: Nguyen Van Loc\n");
+    LOG_IT("Msv: 21T1080019\n");
+    LOG_DT("Xau Q: "); 
+    string s;
+    cin >> s;
+	LOG_DT("Khoa k: ");
+    int k;
+    cin >> k;
+    for(int i=k-1;i>=0;i--)
+	cout<<j[i];
+	for(int i=j.length()-1;i>=k;i--)
+	cout<<j[a];
+	LOG_DT("Xau S: ");	LOG_E("\n");
+}
