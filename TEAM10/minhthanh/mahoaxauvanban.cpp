5c62778ee57d397e543de9ad168d4c54e1485e20
katori3008 <97584303+katori3008@users.noreply.github.com>
2022-05-21 17:52:12 +0700

    asad

--- TEAM10/minhthanh/mahoaxauvanban.cpp
+++ TEAM10/minhthanh/mahoaxauvanban.cpp
@@ -6,12 +6,12 @@ int main(){
     LOG_WT("Bai: Ma Hoa Xau Van Ban\n");
     LOG_IT("Ten: Nguyen Hoang Quang Minh Thanh\n");
     LOG_IT("Msv: 21T1080040\n");
-    LOG_DT(" Xau Q :");
+    LOG_DT("Xau Q :");
     string thanh;
     cin>>thanh;
+    LOG_DT("Khoa k:");
     int k;
     cin>>k;
-    LOG_DT("Khoa k:");
     string a="";
     string b="";
     string ans="";
